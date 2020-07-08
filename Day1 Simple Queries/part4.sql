f112794282b2f8f08f066a6a6312045557804365
StephSteve <stephanie.oncallbusiness@gmail.com>
2020-07-08 19:18:28 -0400

    more updates

--- Day1 Simple Queries/part4.sql
+++ Day1 Simple Queries/part4.sql
@@ -1,2 +1,3 @@
 /*Select all fields from the payments table for payments made on or after January 1, 2005.*/
-SELECT * FROM payments WHERE paymentDate >= 2005-01-01 ORDER BY paymentDate ASC;
+SELECT * FROM payments WHERE paymentDate BETWEEN '2005-01-01' AND '2020-07-08'
+ ORDER BY paymentDate ASC;
