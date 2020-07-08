6e587b065e16f1b79a6044de624d25fc2896babd
mondira-roy2020 <mondira.roy.git2020@gmail.com>
2020-07-08 17:17:49 -0400

    Query assignment completed

--- Mondira_Roy.sql
+++ Mondira_Roy.sql
@@ -16,12 +16,30 @@ select contactFirstName as 'First Name',contactLastName as 'Last Name',city as '
 order by contactLastName;
 
 /*Part-3*/
+select DISTINCT status from orders;
 
+SELECT DISTINCT(status) AS Status
+FROM orders 
+ORDER BY status ASC;
 
 /*Part-4*/
+select * from payments where paymentDate > '2005-01-01';
 
+select * from payments where paymentDate > '2005-01-01' order by paymentDate;
 
 /*Part-5*/
+select lastName, firstName,email,jobTitle from employees Inner Join offices on employees.officeCode = offices.officeCode
+where city='San Francisco' ;
 
+select lastName, firstName,email,jobTitle from employees Inner Join offices on employees.officeCode = offices.officeCode
+where city='San Francisco' order by lastName;
 
 /*Part-6*/
+select productName,ProductLine,productScale, productVendor from products where productLine LIKE '%cars';
+
+select productName,productLine,productScale, productVendor 
+from products 
+where productLine LIKE '%cars'
+GROUP BY productLine
+ORDER BY productName;
+
